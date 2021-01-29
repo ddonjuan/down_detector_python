55b5a7b5219d502c439acafc95adc892359c002b
ddonjuan <daniel.donjuan25@gmail.com>
2021-01-29 00:08:16 -0800

    Made a function that test whether an IP Address is active or not and returns boolean. Tested it on own IP Address in the main.py file

--- down_detector_functions/ip_address_functions.py
+++ down_detector_functions/ip_address_functions.py
@@ -2,6 +2,25 @@ import socket
 import subprocess
 import platform
 
+
+# grabs users IP Address
 def user_ip_address():
     user_ip = socket.gethostbyname(socket.gethostname())
     return user_ip
+
+'''
+**Function will take any IP Address and ping it to see if we can get a response
+**Returns True if IP Address is active, False if there is no response
+'''
+def ping_ip(ip_address):
+    try:
+        ping_ip = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower(
+        ) == "windows" else 'c', ip_address), shell=True, universal_newlines=True)
+
+        if 'unreachable' in ping_ip:
+            return False
+        else:
+            return True
+
+    except Exception:
+        return False
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from down_detector_functions.ip_address_functions import user_ip_address
+from down_detector_functions.ip_address_functions import user_ip_address, ping_ip
 
+
+#Test whether user's IP Address is active
+def test_ip_ping():
+    user_ip = user_ip_address()
+    ping_response = ping_ip(user_ip)
+
+    if ping_response:
+        print("The IP Address has been reached: ", str(user_ip))
+    else:
+        print("The IP Address was unreachable: ", str(user_ip))
 
 #function that executes the program
 def run_down_detector():
     print("Program running....")
-    user_ip = user_ip_address()
-    print("Your IP Address: ", user_ip)
+    test_ip_ping()
+
 
 if __name__ == '__main__':
     run_down_detector()
